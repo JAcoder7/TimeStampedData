4faf2b8881dd56ab93b9f2265b2f3d29cbf9e387
JAcoder7 <jaho24@outlook.de>
2024-02-16 06:54:47 +0100

    serialization prototype

--- src/index.js
+++ src/index.js
@@ -1,79 +1,206 @@
 // @ts-check
 
+const TValuePrimitives = ["string", "number", "boolean", "null"];
+
+/**
+ * @typedef {TMap | string | number | boolean | null} Tvalue
+ */
+
 export class TMap {
-    /** @private @type { Map<string, [TMap | string | number | boolean | null, Date | null]>} */
-    map = new Map();
-    /** @type {Map<string, Date>} */
-     softDeleted = new Map()
-  
-    /**
-     * 
-     * @param {string} key 
-     * @param {TMap | string | number | boolean | null} value 
-     * @returns {TMap}
-     */
-    set(key, value) {
-      if (typeof value != "number"
-        && typeof value != "string"
-        && typeof value != "boolean"
-        && !(value instanceof TMap)
-        && value != null) {
+  /** @private @type { Map<string, [Tvalue, Date]>} */
+  map = new Map();
+  /** @type {Map<string, Date>} */
+  softDeleted = new Map()
+
+  /**
+   * @param {[string, [Tvalue, Date]][]} [entries]
+   */
+  constructor(entries) {
+    if (entries) {
+      this.map = new Map(entries)
+    }
+  }
+
+
+  /**
+   * 
+   * @param {string | null} key 
+   * @param {TMap | string | number | boolean | null} value 
+   * @returns {TMap}
+   */
+  set(key, value) {
+    if (key == null) {
+      key == crypto.randomUUID();
+    }
+
+    switch (true) {
+      case ["number", "string", "boolean"].includes(typeof value):
+      case value === null:
+        break;
+
+      case (typeof value == "object" && value instanceof TMap):
+        // TODO: set parent etc...
+        break;
+
+      default:
         throw new TypeError()
-      }
-  
-      this.map.set(String(key), [value, new Date()])
-  
-      return this
     }
 
-    /** @param {string} key */
-    get(key) {
-      return this.map.get(String(key))?.at(0)
-    }
-  
-    /** @param {string} key */
-    getTimestamp(key) {
-      return this.map.get(String(key))?.at(1)
-    }
-  
-    /** @param {string} key */
-    has(key) {
-      return this.map.has(String(key))
-    }
-  
-    /** @param {string} key */
-    delete(key) {
-      let res = this.map.delete(String(key))
-      if (res) {
-        this.softDeleted.set(String(key), new Date())
-      }
-      return res;
-    }
+    this.map.set(String(key), [value, new Date()])
 
-    get size() {
-      return this.map.size
+    return this
+  }
+
+  /** @param {string} key */
+  get(key) {
+    return this.map.get(String(key))?.at(0)
+  }
+
+  /** @param {number} index */
+  getAtIndex(index) {
+    return this.map.get(String([...this.map.keys()][index]))?.at(0)
+  }
+
+  keys() {
+    return this.map.keys()
+  }
+
+
+  /** @param {string} key */
+  getTimestamp(key) {
+    // TODO: maybe not working
+    return this.map.get(String(key))?.at(1)
+  }
+
+  /** @param {string} key */
+  has(key) {
+    return this.map.has(String(key))
+  }
+
+  /** @param {string} key */
+  delete(key) {
+    let res = this.map.delete(String(key))
+    if (res) {
+      this.softDeleted.set(String(key), new Date())
     }
-  
-    toJSON() {
-      return Object.fromEntries(this.map.entries())
-  
-      console.log({
-        map: Object.fromEntries(this.map.entries()),
-        softDeleted: Object.fromEntries(this.softDeleted.entries())
-      });
-  
-      return {
-        map: Object.fromEntries(this.map.entries()),
-        softDeleted: Object.fromEntries(this.softDeleted.entries())
+    return res;
+  }
+
+  get size() {
+    return this.map.size
+  }
+
+  toFormattedString() {
+    return JSON.stringify(this.toJSON(null, true), null, "  ").replace(/((?<=\[)\n *)|(\n *(?=]))|((?<=,)\n *(?=\d))/g, "").replace(/,/g, ", ")
+  }
+
+  // @ts-expect-error
+  toJSON(_, includeTimestamps = true) {
+    return Object.fromEntries([...[...this.map.entries()].map(([key, value]) => {
+      if (includeTimestamps) {
+        return [key,
+          [value[0] instanceof TMap ? value[0].toJSON(null, includeTimestamps) : value[0], value[1]?.getTime() || 0]
+        ]
+      } else {
+        return [key, value[0] instanceof TMap ? value[0].toJSON(null, includeTimestamps) : value[0]]
       }
-    }
-  
-    /** @param {string} obj */
-    static fromObject(obj) { }
+    }), ...(includeTimestamps?[...this.softDeleted.entries()].map(([key,value])=>[key,{removed:value.getTime()}]):[])
+  ])
   }
-  
-  class Reference {
-    constructor() {
-  
+
+  typeCheck() {
+    // TODO
+  }
+
+  /** 
+ * @param {Object<string,Tvalue|object>} obj
+ * @return {TMap}
+ */
+  static fromJSON(obj) {
+    // @ts-ignore
+    return new TMap(Object.entries(obj).map(/** @param {[string, [string | number | boolean | object | null, number]]} */([key, value]) => {
+     let entry =(Array.isArray(value) && value.length == 2 && [...TValuePrimitives, "object"].includes(typeof value[0]) && typeof value[1] == "number");
+     let isRemoved = typeof value == "object" && typeof value.removed == "number"
+      if (!) {
+        throw new TypeError(`Type "${typeof (value)}" not supported (in {"${key}": ${value})}`)
+      }
+
+
+      if (value[0] != null && typeof value[0] == "object") {
+        // @ts-ignore
+        value[0] = TMap.fromJSON(value[0])
+      }
+
+      return [String(key), [value[0], new Date(value[1])]]
+    }))
+  }
+
+
+  /** 
+   * @param {Object<string,Tvalue|object>} obj
+   * @return {TMap}
+   * @example 
+   * TMap.fromObject({
+        a: 1,
+        b: 2,
+        c: {
+            c_a: [ // arrays will be given keys with crypto.randomUUID()
+              { "a": 3 },
+              { "a": 3 },
+            ]
+        }
+    })
+   */
+  static fromObject(obj) {
+    // @ts-ignore
+    return new TMap(Object.entries(obj).map(([key, value]) => {
+      // @ts-ignore
+      if (value && typeof value == "object" && typeof value.toJSON == "function") {
+        // @ts-ignore
+        value = value.toJSON()
+      }
+      if (value && typeof value == "object") {
+        if (Array.isArray(value)) {
+          value = Object.fromEntries(value.map(v => [crypto.randomUUID(), v]))
+        }
+        // @ts-ignore
+        value = TMap.fromObject(value)
+      } else if (!TValuePrimitives.includes(typeof value)) {
+        throw new TypeError(`Type "${typeof (value)}" not supported (in {"${key}": ${value})}`)
+      }
+      return [String(key), [value, new Date(0)]]
+    }))
+  }
+}
+
+class Reference {
+  path = "";
+
+  /** @param  {...string} keys */
+  constructor(...keys) {
+    this.path = keys.map(k => String(k).replace(/\//g, "\\/")).join("/")
+
+    if (arguments.length == 1 && typeof keys[0] == "string") {
+
     }
   }
+
+  /** @param {TMap} map */
+  eval(map) {
+
+  }
+}
+
+/**
+ * 
+ * @param {object} value 
+ * @param {Function[]} constructors 
+ */
+function isInstanceOf(value, constructors) {
+  for (const constr of constructors) {
+    if (value instanceof constr) {
+      return true
+    }
+  }
+  return false
+}
--- src/index.test.js
+++ src/index.test.js
@@ -7,9 +7,58 @@ test("core", () => {
     expect(TMap).toBeDefined()
     let map = new TMap()
     map.set("1", 5)
+
     // @ts-expect-error
     map.set(6, 5)
+    map.set("7", null)
+    map.set("8", true)
+
+    try {
+        // @ts-expect-error
+        expect(map.set("6", { "a": 5 })).toThrow()
+    } catch (error) { }
+    try {
+        // @ts-expect-error
+        expect(map.set("6", undefined)).toThrow()
+    } catch (error) { }
+    try {
+        // @ts-expect-error
+        expect(map.set("6",)).toThrow()
+    } catch (error) { }
+
     expect(map.delete("1")).toBeTrue()
-    expect(map.size).toBe(1)
+    expect(map.size).toBe(3)
     console.log(map);
+})
+
+test("serialization", () => {
+    let map = TMap.fromObject({
+        users: [
+            {name: "Test User 1"},
+            {name: "Test User 2"}
+        ],
+        b: 2,
+        c: {
+            ca: new Date(),
+            cb: [{ "a": 3 }, "b"]
+        }
+    });
+
+    /** @type {TMap} */(map.get("users"))?.delete([...map.get("users").keys()][0])
+    console.log(map.get("users").get([...map.keys()][0]));
+    
+    //map.set("d", 4)
+
+    //console.log(map);
+
+    // let json = map.toJSON()
+    // let map2 = TMap.fromJSON(json)
+    // let json2 = map2.toJSON()
+            
+    // expect(JSON.stringify(json)).toBe(JSON.stringify(json2))
+    // expect(JSON.stringify(map.toJSON(false))).toBe(JSON.stringify(map2.toJSON(false)))
+
+    console.log("hä");
+    console.log("from Object: ", map.toFormattedString());
+    //console.log("from Object: ", map.toJSON(true, true));
 })
